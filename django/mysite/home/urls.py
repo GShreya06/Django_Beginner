f27ab7056aadd91b6a6833338deaae63cc20cae6
Shreya <shreyagupta4563@gmail.com>
2022-09-17 20:35:35 +0530

    Added file

--- django/mysite/home/urls.py
+++ django/mysite/home/urls.py
@@ -4,8 +4,8 @@ from home import views
 
 
 urlpatterns = [
-    path('',views.index, name="index"),
-    path("about/",views.about, name="about"),
-    path("services/",views.services, name="services"),
-    path("contact/",views.contact, name="contact")
+    path("",views.index, name='home'),
+    path("about/",views.about, name='about'),
+    path("services/",views.services, name='services'),
+    path("contact/",views.contact, name='contact'),
 ]
--- django/mysite/home/views.py
+++ django/mysite/home/views.py
@@ -1,21 +1,35 @@
+from ast import Name
+from datetime import datetime
 from multiprocessing import context
+from urllib.request import Request
 from django.shortcuts import render , HttpResponse
-context={
-        "variable":"This is variable"
-}
+from datetime import datetime
+from home.models import Contact
+
+
+# context={
+#         "variable":"This is variable"
+# }
 # Create your views here.
 def index(request):
-    return render(request,'index.html',context)
+    return render(request,'index.html')
     #return HttpResponse("This is homePage")
 def about(request):
-    return render(request,'about.html',context)
+    return render(request,'about.html')
     #return HttpResponse("This is About Page")
 
 def services(request):
-    return render(request,'services.html',context)
+    return render(request,'services.html')
     #return HttpResponse("This is services Page")
 
 def contact(request):
-    return render(request,'contact.html',context)
+    if request.method == "post":
+        name =Request.post.get('name')
+        email = Request.post.get('email')
+        phone = Request.post.get('phone')
+        desc = Request.post.get('desc')
+        contact = Contact(name=name , email= email , phone =phone , desc =desc,date=datetime.today())
+        contact.save()
+    return render(request,'contact.html')
     #return HttpResponse("This is Contact Page")
 
